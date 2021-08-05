a21abd901b4deeaad1f8fcf984c212ca067fea88
ShimonSamay <shimonsamay7@gmail.com>
2021-08-05 11:48:01 +0300

    fix

---
+++ main.js
@@ -0,0 +1,216 @@
+
+//exe 1
+
+// var num1=Number(prompt("type first number"));
+// var num2=Number(prompt("type second number"));
+// var result=num1+num2
+
+// console.log(`number one is${num1}`);
+// console.log(`number two is${num2}`);
+
+// if(result>=13){
+//     console.log("this is bar miztva")
+// }  else if(result==12){
+//     console.log("this is batmizva")
+// } else{
+//     console.log("this is a kid")
+// }
+
+
+//exe2
+
+// var round=0
+// while(round<100){
+//     round++
+//     console.log("yes")
+// }
+
+//exe3
+// var x=0
+// var userInput=Number(prompt("type a number"));
+// while(userInput>x){
+//     x++
+//     console.log("no")
+
+// }
+
+//exe4
+
+// for(var i=0; 1000>=i; i++){
+//     console.log("javascript is not a java")
+// }
+
+//exe5
+
+// var number=Number(prompt("type a number"))
+// var Name=prompt("type a name")
+// for(var i=0; number>i ; i++){
+//     console.log(Name)
+// }
+
+
+// // exe6
+// var firstname=prompt("type first name");
+// var age=Number(prompt("type an age"));
+// var email=prompt("type an email");
+// console.log(`my first name${firstname}. \n  my age is${age}. \n my email is${email}`)
+
+// var usernumber= Number(prompt("type a number"));
+// for(var i=0; i<usernumber; i++ ){
+//     if(age>18){
+//         console.log(firstname+age+email)
+
+//     }
+// }
+
+
+//exe7
+
+// var numofUser=Number(prompt("type a number"))
+// for(var i=0; i<numofUser; i++){
+//     var secondname=prompt("type a name")
+//     if(firstname==secondname){
+//         console.log(`hi boss ${firstname}`);
+//     }else{
+//         console.log("no entry");
+//         break;
+//     }
+// }
+
+
+//exe8
+// var num1=Number(prompt("type first number"))
+// var num2=Number(prompt("type second number"))
+// for(var i=0; i<num1; i++){
+//     for(var j=0; j<num2; j++){
+//         console.log(i*j)
+//     }
+// }
+
+
+// //exe9
+// var array=[8,"shimon",9,"samay"]
+// console.log(array)
+
+//exe10
+// var myarry=[8,9,4,2,6];
+// console.log(myarry)
+// for(var i=0; i<myarrr)
+
+
+//exe11
+
+// var names=["shimon","amir","avi","yoni","oshri"]
+// console.log(names[0]);
+// console.log(names[1]);
+// console.log(names[2]);
+// console.log(names[3]);
+// console.log(names[4]);
+
+//exe12
+
+// var myArry=[22,89,54,7,11,8,99,43]
+// for(var i=0; i<myArry.length; i++){
+//     if(myArry[i]%2 ==0){
+//         console.log(myArry[i])
+    
+//     }
+// }
+
+// exe13
+// var namesFromUser=[]
+// for(var i=0; i<5; i++){
+//     var namesFromUser=prompt("type a Name");
+//     console.log(namesFromUser);
+// }
+
+//exe14
+// var userNums=[]
+// for(var i=0; i<5; i++){
+//    userNums[i]= Number(prompt("type a number"));
+//   if(userNums[i]%2!=0){
+//       console.log(userNums[i])
+//   }
+
+// }
+
+
+
+
+// exe15
+// var random=["hey","my","name", "is","shimon", 19, 22,24]
+// for(var i=0; i<random.length; i++){
+//     if(random[i]/1!==random[i]){
+//         random[i]=("not a number")
+
+//     }
+
+// }
+// console.log(random)
+
+
+//exe16
+// var userArray=[]
+// var round=0
+// while(round<10){
+//     var userChoice =Number(prompt("type a number"));
+//     if(userChoice%2==0){
+//         userArray[round]=userChoice;
+//         round++
+//     }
+// }
+// console.log(userArray)
+
+
+//exe17
+
+// var width
+// var height
+
+// width=Number(prompt("type width"))
+// height=Number(prompt("type height"))
+// console.log(width*height)
+
+//exe15
+// var numOne
+// var numTwo
+// var numThree
+
+// numOne=Number(prompt("type first number"))
+// numTwo=Number(prompt("type second number"))
+// numThree=Number(prompt("type third number"))
+
+// if(numOne>numTwo && numOne>numThree && numTwo>numThree){
+//     console.log("Downside")
+// } 
+// else if(numThree>numOne && numThree>numTwo && numTwo>numOne){
+//     console.log("Upside")
+// }
+// else{
+//     console.log("no order")
+// }
+
+
+//exe16
+// var UserPositiveNumber=Number(prompt("type a number"));
+// var firstArray=[]
+
+// for(var i=0; i<UserPositiveNumber.toString().length; i++) {
+  
+//     if(UserPositiveNumber>0){
+//         firstArray[i]=UserPositiveNumber.toString().charAt(i)
+//         console.log(firstArray[i])
+//     }
+
+// }
+
+
+//exe17
+
+// for (var i=0; i<5; i++){
+
+//     for (var k=0; k<100; k++){
+//         console.log("yes")
+//     }
+// }
+
